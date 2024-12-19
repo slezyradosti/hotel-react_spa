79dbf1607fb32e2971fc739eb328fb9cbbffb17e
Life is squid <rredzer000@gmail.com>
2024-12-19 23:14:05 +0000

    update app, use router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import { HotelServiceContext } from './context/hotelServiceContext';
 import HotelList from './pages/HotelList'
 import { HotelService } from './services/hotelService';
+import HotelDetails from "./pages/HotelDetails";
 
 function App() {
   const hotelService = new HotelService();
 
   return (
-    <HotelServiceContext.Provider value={hotelService}>
-      <HotelList />
-    </HotelServiceContext.Provider>
+    <Router>
+      <HotelServiceContext.Provider value={hotelService}>
+        <Routes>
+          <Route path="/" element={<HotelList />} />
+          <Route path="/hotel/:id" element={<HotelDetails />} />
+        </Routes>
+      </HotelServiceContext.Provider>
+    </Router>
   );
 }
 
